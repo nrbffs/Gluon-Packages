8423c8106fec8dee3ec8a5af8f5636b2f85e86f5
Flip <philippe@pilarkto.net>
2017-10-26 09:34:14 +0200

    convert zip packages to gluon-web

--- gluon-config-mode-zip/luasrc/lib/gluon/config-mode/wizard/0450-geo-location.lua
+++ gluon-config-mode-zip/luasrc/lib/gluon/config-mode/wizard/0450-geo-location.lua
@@ -1,27 +1,17 @@
-local cbi = require "luci.cbi"
-local i18n = require "luci.i18n"
-local uci = require("simple-uci").cursor()
 
-local M = {}
+return function(form, uci)
+    local location = uci:get_first("gluon-node-info", "location")
+    local text =  translate("gluon-config-mode:zip-help")
+    
+    local s = form:section(Section, nil, text)
 
-function M.section(form)
-    local text = i18n.translate("gluon-config-mode:zip-help")
-    local fieldlabel = i18n.translate("ZIP-Code")
-    local s = form:section(cbi.SimpleSection, nil, text)
-
-    local o = s:option(cbi.Value, "_zip", fieldlabel)
-    o.value = uci:get_first("gluon-node-info", "location", "zip")
-    o.rmempty = true
+    local o = s:option(Value, "zip", translate("ZIP-Code"), translatef("e.g. %s", "70499"))
+    o.default = uci:get("gluon-node-info", location, "zip")
     o.datatype = "uinteger"
-    o.description = i18n.translatef("e.g. %s", "70499")
+    o.optional = true
+    function o:write(data)
+        uci:set("gluon-node-info", location, "zip", data)
+    end
+    
+    return {'gluon-node-info'}
 end
-
-function M.handle(data)
-    local sname = uci:get_first("gluon-node-info", "location")
-
-    uci:set("gluon-node-info", sname, "zip", data._zip:trim())
-    uci:save("gluon-node-info")
-    uci:commit("gluon-node-info")     
-end
-        
-return M
